e638de776c6855d23f8c36b6699eee916e0e9d73
Unfield <gpgvip@gmail.com>
2023-01-04 21:43:20 +0100

    removed the if(browser) check

--- svelteSnow.svelte
+++ svelteSnow.svelte
@@ -1,15 +1,13 @@
-<script>import { browser } from "$app/environment";
+<script>
 import "./svelteSnow.css";
 export let active = true;
 export let activeMonths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-if (browser) {
-  if (active) {
-    let currentMonth = new Date().getMonth() + 1;
-    if (activeMonths.includes(currentMonth))
-      active = true;
-    else
-      active = false;
-  }
+if (active) {
+  let currentMonth = new Date().getMonth() + 1;
+  if (activeMonths.includes(currentMonth))
+    active = true;
+  else
+    active = false;
 }
 </script>
 
